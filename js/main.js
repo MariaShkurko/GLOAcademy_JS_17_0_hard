9f807d7caf51565440c5f8165c99e0aa6bd03214
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-02-04 10:37:24 +0300

    Added 2 algorithms for searching prime numbers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let arr = [
     '125555594412125451521',
     '254151254510',
@@ -14,8 +16,54 @@ arr.forEach(element => {
     }
 });
 
-let simpleNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+let simple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-simpleNumbers.forEach(element => {
+const simpleNumbers = function (n) {  
+    let simpleNumbers = [2];
+
+    nextNumber: 
+    for (let i = 3; i <= n; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                continue nextNumber;
+            }
+        }
+        simpleNumbers.push(i);
+    }
+
+    return simpleNumbers;
+};
+
+const sieveOfEratosthenes = function (n) {
+    let simpleNumbers = [];
+
+    for (let i = 2; i <= n; i++) {
+        simpleNumbers.push(i);
+    }
+
+    for (let j = 0; j < simpleNumbers.length; j++) {
+        let arr = simpleNumbers;
+
+        for (let i = j + 1; i < simpleNumbers.length; i++) {
+            if (simpleNumbers[i] % simpleNumbers[j] === 0) {
+                arr.splice(i, 1);
+            }
+        }
+
+        simpleNumbers = arr;
+    }
+
+    return simpleNumbers;
+};
+
+console.log('');
+console.log('Сравнение результатов эталонного массива, алгоритма простого перебора и алгоритма Решето Эратосфена');
+console.log(simple);
+console.log(simpleNumbers(100));
+console.log(sieveOfEratosthenes(100));
+
+console.log('');
+console.log('Вывод результатов алгоритма Решето Эратосфена в требуемом формате');
+sieveOfEratosthenes(100).forEach(element => {
     console.log(element + '\tДелители этого числа: 1 и ' + element);
 });
