3f905131438f68b94b40540d8decfb0f7dde05ae
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-02-05 16:47:38 +0300

    Lesson 7 hard is completed

--- js/main.js
+++ js/main.js
@@ -1,79 +1,30 @@
 'use strict';
 
-let arr = [
-    '125555594412125451521',
-    '254151254510',
-    '151521245231513515',
-    '454154854536',
-    '701214521212311',
-    '249885210.53',
-    '535416355211525415684'
+let week = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
 ];
 
-arr.forEach(element => {
-    if (element[0] == '2' || element[0] == '4') {
-        console.log(element);
-    }
-});
+let currentDayOfWeek = (new Date()).getDay() - 1;
+currentDayOfWeek = currentDayOfWeek === (-1) ? 6 : currentDayOfWeek;
 
-let simple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+console.log(week[currentDayOfWeek]);
 
-const simpleNumbers = function (n) {  
-    let simpleNumbers = [2];
+for (const day of week) {
+    let style = '';
 
-    nextNumber: 
-    for (let i = 3; i <= n; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                continue nextNumber;
-            }
-        }
-        simpleNumbers.push(i);
+    if (day === week[currentDayOfWeek]) {
+        style = 'font-weight: bold; ';
     }
 
-    return simpleNumbers;
-};
-
-const sieveOfEratosthenes = function (n) {
-    let simpleNumbers = [];
-
-    for (let i = 2; i <= n; i++) {
-        simpleNumbers.push(i);
+    if (day === week[5] || day === week[6]) {
+        style += 'font-style: italic; ';
     }
 
-    for (let j = 0; j < simpleNumbers.length; j++) {
-        if (simpleNumbers[j] > n/2) {
-            break;
-        }
-
-        let arr = simpleNumbers;
-
-        for (let i = j + 1; i < simpleNumbers.length; i++) {
-            if (simpleNumbers[i] % simpleNumbers[j] === 0) {
-                arr.splice(i, 1);
-            }
-        }
-
-        simpleNumbers = arr;
-    }
-
-    return simpleNumbers;
-};
-
-console.log('');
-console.log('Сравнение результатов эталонного массива, алгоритма простого перебора и алгоритма Решето Эратосфена');
-console.log(simple);
-
-console.time('checkFor');
-console.log(simpleNumbers(100));
-console.timeEnd('checkFor');
-
-console.time('checkFor');
-console.log(sieveOfEratosthenes(100));
-console.timeEnd('checkFor');
-
-console.log('');
-console.log('Вывод результатов алгоритма Решето Эратосфена в требуемом формате');
-sieveOfEratosthenes(100).forEach(element => {
-    console.log(element + '\tДелители этого числа: 1 и ' + element);
-});
+    console.log('%c%s', style, day);
+}
